1d242e7ad0c5ab55a2e3de3a458bf34d9973e079
jonathanmartirubio <jonamr09@gmail.com>
2022-04-09 21:17:33 +0200

    Excepciones y métodos de prueba refactorizados

--- Bank/Bank/BankAccountJMR2122.cs
+++ Bank/Bank/BankAccountJMR2122.cs
@@ -25,6 +25,10 @@ namespace BankAccountNS
         {
             get { return m_balance; }
         }
+
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount less than zero";
+
         public void Debit(double amount)
         {
             if (m_frozen)
@@ -33,11 +37,11 @@ namespace BankAccountNS
             }
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
             m_balance -= amount; // intentionally incorrect code
         }
